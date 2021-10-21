9796a66ac6e337471f4ed7170b0ab0b28b3743a8
Devin <devin@jcube.com.cn>
2021-10-21 09:48:39 +0800

    删除 src/ohpTips 文件夹，将内容移动到 src/

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // 指定构建模式
-  entry: './src/ohoTips/js/ohoTips.js',
+  entry: './src/js/ohoTips.js',
   output: {
     filename: 'ohoTips.js',
     path: path.resolve(__dirname, 'dist/ohoTips/js'),
