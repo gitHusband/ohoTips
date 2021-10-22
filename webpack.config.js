a299913659392d5890fd6913fb3d21caa80f7a59
Devin <devin@jcube.com.cn>
2021-10-22 15:04:20 +0800

    智能选择打包JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
+const glob = require('glob');
+
+const OHO_PATH = {
+  js: {
+    pattern: './src/js/[^_]*.js',
+    src: path.join(__dirname, 'src/js'),
+    dst: path.resolve(__dirname, 'dist/ohoTips/js'),
+  }
+}
+
+// 遍历除所有需要打包的JS文件路径
+function getEntries(config) {
+  let fileList = glob.sync(config.pattern);
+  return fileList.reduce(function (previous, current) {
+    let filePath = path.parse(path.relative(config.src, current));
+    let withoutSuffix = path.join(filePath.dir, filePath.name);
+    previous[withoutSuffix] = path.resolve(__dirname, current);
+    return previous;
+  }, {});
+}
+console.log(getEntries(OHO_PATH.js));
 
 module.exports = {
   mode: 'development', // 指定构建模式
-  entry: './src/js/ohoTips.js',
+  entry: getEntries(OHO_PATH.js),
+  // entry: {
+  //   ohoTips: './src/js/ohoTips.js',
+  //   common: './src/js/common.js'
+  // },
   output: {
-    filename: 'ohoTips.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist/ohoTips/js'),
     library: {
       name: 'oho',
